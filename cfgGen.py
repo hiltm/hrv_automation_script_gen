85e709b8a011ab173980cab93de271ec67bebf23
Michael Hilt <49827546+hiltm@users.noreply.github.com>
2023-08-03 15:05:03 -0400

    reformatting how flush function called, breaking out min and max values for inputs as global to make it easier to modify in future

--- cfgGen.py
+++ cfgGen.py
@@ -17,6 +17,18 @@
 #Number of incubation studies to be ran
 
 #### PARAMETER LIMITS ####
+## initial setup ##
+incubatorVolume_min = 0 #mL
+incubatorVolume_max = 2000 #mL
+injectorVolume_min = 0 #mL
+injectorVolume_max = 250 #mL
+## flush ##
+flushCycles_min = 0
+flushCycles_max = 5
+flushWaittime_min = 5 #sec
+flushWaittime_max = 300 #sec
+flushAmount_min = 0 #mL
+flushAmount_max = 2000 #mL
 
 
 import sys
@@ -39,6 +51,32 @@ def int_check(parameter, min_value, max_value):
             else:
                 valid_answer = True
                 return number1
+            
+def flush():
+    print("Specify flush cycles for incubator, range is between "+str(flushCycles_min)+" and "+str(flushCycles_max))
+    flush_cycles = int_check("flush_cycles", flushCycles_min, flushCycles_max)
+    print("Specify wait time in seconds between flush cycles, range is between "+str(flushWaittime_min)+" and "+str(flushWaittime_max))
+    flush_waittime = int_check("flush_waittime", flushWaittime_min, flushWaittime_max)
+    print("Specify flush amount in mL, range is between "+str(flushAmount_min)+" and "+str(flushAmount_max))
+    flush_amount = int_check("flush_amount", flushAmount_min, flushAmount_max)
+    f.write("#Incubator pre-flush")
+    f.write("\r")
+    f.write("wHp")          #wait for home port
+    f.write("\r")
+    f.write("gSp")          #get a prompt from the SID
+    f.write("\r")
+    f.write("eP")           #completely empty incubator
+    f.write("\r")
+    f.write("rP:"+str(flush_cycles))    #loop flush_cycles times
+    f.write("\r")
+    f.write("fV:"+str(flush_amount))     #flush amount
+    f.write("\r")
+    f.write("wS:"+str(flush_waittime))   #wait for flush_waittime seconds
+    f.write("\r")
+    f.write("eP")                   # completely empty incubator
+    f.write("\r")
+    f.write("eRpn")                 # end loop
+    f.write("\r")
     
     
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -51,42 +89,19 @@ with open(filename, "w") as f:
 #try:
     print("Initial configutation/first-time setup")
     print("#####################################")
-    print("Specify actual total incubator volume in mL")
-    iV=int_check("iV", 0, 2000)
+    print("Specify actual total incubator volume in mL, range is between "+str(incubatorVolume_min)+" and "+str(incubatorVolume_max))
+    iV=int_check("iV", incubatorVolume_min, incubatorVolume_max)
     f.write("iV:"+str(iV)+"\r")
-    print("Specify actual total injector volume in mL")
-    tV=int_check("tV", 0, 500)
+    print("Specify actual total injector volume in mL, range is between "+str(injectorVolume_min)+" and "+str(injectorVolume_max))
+    tV=int_check("tV", injectorVolume_min, injectorVolume_max)
     f.write("tV:"+str(tV)+"\r")
     print("TODO any more init cfg params")
     print(" ")
     print("#####################################")
     print("Incubator Pre-Flush Parameters")
     print("#####################################")
-    print("Specify flush cycles for incubator")
-    flush_cycles = int_check("flush_cycles", 0, 5)
-    print("Specify wait time in seconds between flush cycles, min. 5 seconds")
-    flush_waittime = int_check("flush_waittime", 5, 300)
-    print("Specify flush amount in mL")
-    flush_amount = int_check("flush_amount", 5, 300)
-    def flush():
-        f.write("#Incubator pre-flush")
-        f.write("\r")
-        f.write("wHp")          #wait for home port
-        f.write("\r")
-        f.write("gSp")          #get a prompt from the SID
-        f.write("\r")
-        f.write("eP")           #completely empty incubator
-        f.write("\r")
-        f.write("rP:"+flush_cycles)     #loop flush_cycles times
-        f.write("\r")
-        f.write("fV:"+flush_amount)     #flush amount
-        f.write("\r")
-        f.write("wS:"+flush_waittime)   #wait for flush_waittime seconds
-        f.write("\r")
-        f.write("eP")                   # completely empty incubator
-        f.write("\r")
-        f.write("eRpn")                 # end loop
-        f.write("\r")
+    flush()
+    
     print("#####################################")
     print("Filtration Test Parameters")
     print("#####################################")
@@ -101,8 +116,7 @@ with open(filename, "w") as f:
         ##wait adjustable time
         ##pump to next port
         ##repeat for up to 24 hours
-    
-    #flush()
+
     f.close()
     
 #except EOFError:
