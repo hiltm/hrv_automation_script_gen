c0572c8c7cbeb96ca88ea17e73c2419f4aa12451
Michael Hilt <49827546+hiltm@users.noreply.github.com>
2023-08-09 15:37:37 -0400

    updating todos, removing some unnecessary comments, and some light formatting

--- cfgGen.py
+++ cfgGen.py
@@ -1,21 +1,7 @@
 ###TODOS#######
-#put limits on input values
-#put error handling
+#check if using injector or not; this will have different commands sent to the PIC
 #create command to get start time
 #find some way to estimate runtime
-
-####PARAMS
-#Sample volume amount
-#Tracer volume amount
-#TIMEZERO start time
-#TIMEZERO ports
-#TIMEZERO time between ports
-#TIMEZERO volume amount
-#Amount of time between TIMEZERO and TIMEONE
-#TIMEONE start time
-#TIMEONE ports
-#TIMEONE time between ports
-#Number of incubation studies to be ran
 
 import os
 import params
@@ -54,7 +40,7 @@ def init_cfg():
     f.write("\r")
     print("Specify actual total incubator volume in mL, range is between "+str(params.incubatorVolume_min)+" and "+str(params.incubatorVolume_max)+". Default is "+str(params.incubatorVolume_dft))
     iV=int_check("iV", params.incubatorVolume_min, params.incubatorVolume_max, params.incubatorVolume_dft)
-    set_intake(iV) # setting global to track intake volume
+    set_intake(iV)                          # setting global to track intake volume
     f.write("iV:"+str(iV)+"\r")
     print("Specify actual total injector volume in mL, range is between "+str(params.injectorVolume_min)+" and "+str(params.injectorVolume_max)+". Default is "+str(params.injectorVolume_dft))
     tV=int_check("tV", params.injectorVolume_min, params.injectorVolume_max, params.injectorVolume_dft)
@@ -66,7 +52,7 @@ def flush():
     f.write("\r")
     print("Specify flush cycles for incubator, range is between "+str(params.flushCycles_min)+" and "+str(params.flushCycles_max)+". Default is "+str(params.flushCycles_dft))
     flush_cycles = int_check("flush_cycles", params.flushCycles_min, params.flushCycles_max, params.flushCycles_dft)
-    f.write("rP:"+str(flush_cycles))   #repeat for flush_cycles times
+    f.write("rP:"+str(flush_cycles))        #repeat for flush_cycles times
     f.write("\r")
     for x in range(flush_cycles):
         f.write("#Flush cycle "+str(x))
@@ -75,11 +61,11 @@ def flush():
         flush_waittime = int_check("flush_waittime", params.flushWaittime_min, params.flushWaittime_max, params.flushWaittime_dft)
         print("Specify flush amount in mL, range is between "+str(params.flushAmount_min)+" and "+str(params.flushAmount_max)+". Default is "+str(params.flushAmount_dft))
         flush_amount = int_check("flush_amount", params.flushAmount_min, params.flushAmount_max, params.flushAmount_dft)
-        f.write("wHp")          #wait for home port
+        f.write("wHp")                      #wait for home port
         f.write("\r")
-        f.write("gSp")          #get a prompt from the SID
+        f.write("gSp")                      #get a prompt from the SID
         f.write("\r")
-        f.write("eP")           #completely empty incubator
+        f.write("eP")                       #completely empty incubator
         f.write("\r")
         f.write("rP:"+str(flush_cycles))    #loop flush_cycles times
         f.write("\r")
@@ -87,16 +73,16 @@ def flush():
         f.write("\r")
         f.write("wS:"+str(flush_waittime))   #wait for flush_waittime seconds
         f.write("\r")
-        f.write("eP")                   # completely empty incubator
+        f.write("eP")                        # completely empty incubator
         f.write("\r")
-        f.write("eRpn")                 # end loop
+        f.write("eRpn")                      # end loop
         f.write("\r")
 
 def incubation():
     f.write("#incubation study")
     f.write("\r")
     print("The incubator chamber will be filled to the total incubator volume")
-    f.write("fV:"+str(get_intake())) # fill incubator chamber to total incubator volume
+    f.write("fV:"+str(get_intake()))        # fill incubator chamber to total incubator volume
     f.write("\r")
     print("Specify amount of incubation chamber volume to be used during incubation study, range is between "+str(params.incubationTestIncubatorDrawVolume_min)+" and "
           +str(params.incubationTestIncubatorDrawVolume_max)+". Default is "+str(params.incubationTestIncubatorDrawVolume_dft)+". This number will be referred to as the OUTTAKE.")
@@ -114,17 +100,6 @@ def incubation():
         f.write("\r")
         ###########TODO port selection command
 
-
-         #incubation_test_cycles = input()
-   # for 1:incubation_test_cycles
-        ##incubation draw volume
-        ##injection draw volume
-        ##CMD pump incubation chamber to HRV
-        ##CMD pump HRV to filters
-        ##at TIMEZERO pump to 1-5 ports
-        ##wait adjustable time
-        ##pump to next port
-        ##repeat for up to 24 hours
     print('filler')
     
 #file generation   
