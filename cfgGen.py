98ab5694b996521db36881814d8a01a5e0099226
Michael Hilt <49827546+hiltm@users.noreply.github.com>
2023-08-09 11:59:20 -0400

    Moved params to separate file called params.py; including default values in case user does not enter parameter

--- cfgGen.py
+++ cfgGen.py
@@ -17,31 +17,7 @@
 #TIMEONE time between ports
 #Number of incubation studies to be ran
 
-#### PARAMETER LIMITS ####
-## initial setup ##
-incubatorVolume_min = 0 #mL
-incubatorVolume_max = 2000 #mL
-injectorVolume_min = 0 #mL
-injectorVolume_max = 250 #mL
-## flush ##
-flushCycles_min = 0
-flushCycles_max = 5
-flushWaittime_min = 5 #sec
-flushWaittime_max = 300 #sec
-flushAmount_min = 0 #mL
-flushAmount_max = 2000 #mL
-## filtration ##
-filtrationTestCycles_min = 0
-filtrationTestCycles_max = 50
-filtrationTestIncubatorDrawVolume_min = 0 #mL
-filtrationTestIncubatorDrawVolume_max = 2000 #mL
-filtrationTestInjectorDrawVolume_min = 0 #mL
-filtrationTestInjectorDrawVolume_max = 2000 #mL
-filtrationTestWaitBetweenPorts_min = 0 #sec
-filtrationTestWaitBetweenPorts_max = 300 #sec
-filtrationTestWaitBetweenStudies_min = 0 #sec
-filtrationTestWaitBetweenStudies_max = 200000 #sec
-
+import params
 
 acceptable_chars = set('0123456789')
 
@@ -49,10 +25,10 @@ acceptable_chars = set('0123456789')
 import sys
 import os
 
-def int_check(parameter, min_value, max_value):
+def int_check(parameter, min_value, max_value, dft_value):
     valid_answer = False
     while not(valid_answer):
-            number1 = input(': ')
+            number1 = input(': ') or dft_value
             #if not(number1 in acceptable_chars):
             #     print('nah')
             #     continue
@@ -70,21 +46,21 @@ def int_check(parameter, min_value, max_value):
                 return number1
             
 def init_cfg():
-    print("Specify actual total incubator volume in mL, range is between "+str(incubatorVolume_min)+" and "+str(incubatorVolume_max))
-    iV=int_check("iV", incubatorVolume_min, incubatorVolume_max)
+    print("Specify actual total incubator volume in mL, range is between "+str(params.incubatorVolume_min)+" and "+str(params.incubatorVolume_max))
+    iV=int_check("iV", params.incubatorVolume_min, params.incubatorVolume_max, params.incubatorVolume_dft)
     f.write("iV:"+str(iV)+"\r")
-    print("Specify actual total injector volume in mL, range is between "+str(injectorVolume_min)+" and "+str(injectorVolume_max))
-    tV=int_check("tV", injectorVolume_min, injectorVolume_max)
+    print("Specify actual total injector volume in mL, range is between "+str(params.injectorVolume_min)+" and "+str(params.injectorVolume_max))
+    tV=int_check("tV", params.injectorVolume_min, params.injectorVolume_max, params.injectorVolume_dft)
     f.write("tV:"+str(tV)+"\r")
     print("TODO any more init cfg params")
             
 def flush():
-    print("Specify flush cycles for incubator, range is between "+str(flushCycles_min)+" and "+str(flushCycles_max))
-    flush_cycles = int_check("flush_cycles", flushCycles_min, flushCycles_max)
-    print("Specify wait time in seconds between flush cycles, range is between "+str(flushWaittime_min)+" and "+str(flushWaittime_max))
-    flush_waittime = int_check("flush_waittime", flushWaittime_min, flushWaittime_max)
-    print("Specify flush amount in mL, range is between "+str(flushAmount_min)+" and "+str(flushAmount_max))
-    flush_amount = int_check("flush_amount", flushAmount_min, flushAmount_max)
+    print("Specify flush cycles for incubator, range is between "+str(params.flushCycles_min)+" and "+str(params.flushCycles_max))
+    flush_cycles = int_check("flush_cycles", params.flushCycles_min, params.flushCycles_max, params.flushCycles_dft)
+    print("Specify wait time in seconds between flush cycles, range is between "+str(params.flushWaittime_min)+" and "+str(params.flushWaittime_max))
+    flush_waittime = int_check("flush_waittime", params.flushWaittime_min, params.flushWaittime_max, params.flushWaittime_dft)
+    print("Specify flush amount in mL, range is between "+str(params.flushAmount_min)+" and "+str(params.flushAmount_max))
+    flush_amount = int_check("flush_amount", params.flushAmount_min, params.flushAmount_max, params.flushAmount_dft)
     f.write("#Incubator pre-flush")
     f.write("\r")
     f.write("wHp")          #wait for home port
@@ -105,8 +81,8 @@ def flush():
     f.write("\r")
 
 def filtration():
-    print("Specify amount of filtration cycles to be completed, range is between "+str(filtrationTestCycles_min)+" and "+str(filtrationTestCycles_max))
-    filtration_test_cycles = int_check("flush_cycles", filtrationTestCycles_min, filtrationTestCycles_max)
+    print("Specify amount of filtration cycles to be completed, range is between "+str(params.filtrationTestCycles_min)+" and "+str(params.filtrationTestCycles_max))
+    filtration_test_cycles = int_check("flush_cycles", params.filtrationTestCycles_min, params.filtrationTestCycles_max, params.filtrationTestCycles_dft)
     f.write("#Filtration study")
 
          #filtration_test_cycles = input()
---
+++ params.py
@@ -0,0 +1,34 @@
+#### PARAMETER LIMITS ####
+## initial setup ##
+incubatorVolume_min = 0 #mL
+incubatorVolume_max = 2000 #mL
+incubatorVolume_dft = 2000 #mL
+injectorVolume_min = 0 #mL
+injectorVolume_max = 250 #mL
+injectorVolume_dft = 0 #mL
+## flush ##
+flushCycles_min = 0
+flushCycles_max = 5
+flushCycles_dft = 1
+flushWaittime_min = 5 #sec
+flushWaittime_max = 300 #sec
+flushWaittime_dft = 30 #sec
+flushAmount_min = 0 #mL
+flushAmount_max = 2000 #mL
+flushAmount_dft = 2000 #mL
+## filtration ##
+filtrationTestCycles_min = 0
+filtrationTestCycles_max = 50
+filtrationTestCycles_dft = 10
+filtrationTestIncubatorDrawVolume_min = 0 #mL
+filtrationTestIncubatorDrawVolume_max = 2000 #mL
+filtrationTestIncubatorDrawVolume_max = 1000 #mL
+filtrationTestInjectorDrawVolume_min = 0 #mL
+filtrationTestInjectorDrawVolume_max = 2000 #mL
+filtrationTestInjectorDrawVolume_dft = 0 #mL
+filtrationTestWaitBetweenPorts_min = 0 #sec
+filtrationTestWaitBetweenPorts_max = 300 #sec
+filtrationTestWaitBetweenPorts_dft = 10 #sec
+filtrationTestWaitBetweenStudies_min = 0 #sec
+filtrationTestWaitBetweenStudies_max = 200000 #sec
+filtrationTestWaitBetweenStudies_max = 100 #sec
