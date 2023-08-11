b5b5ad2434189c2911892e83e0ab3c5368e672e0
Michael Hilt <49827546+hiltm@users.noreply.github.com>
2023-08-11 11:48:12 -0400

    updating default parameters based on feedback from scientists

--- params.py
+++ params.py
@@ -1,13 +1,13 @@
 #### PARAMETER LIMITS ####
 ## initial setup ##
 incubatorVolume_min = 0 #mL
-incubatorVolume_max = 2000 #mL
-incubatorVolume_dft = 2000 #mL
+incubatorVolume_max = 1950 #mL
+incubatorVolume_dft = 1950 #mL
 injectorVolume_min = 0 #mL
-injectorVolume_max = 250 #mL
+injectorVolume_max = 500 #mL
 injectorVolume_dft = 0 #mL
 studyCycles_min = 0
-studyCycles_max = 5
+studyCycles_max = 20
 studyCycles_dft = 1
 ## flush ##
 flushCycles_min = 0
@@ -17,17 +17,17 @@ flushWaittime_min = 5 #sec
 flushWaittime_max = 300 #sec
 flushWaittime_dft = 30 #sec
 flushAmount_min = 0 #mL
-flushAmount_max = 2000 #mL
-flushAmount_dft = 2000 #mL
+flushAmount_max = 1950 #mL
+flushAmount_dft = 1950 #mL
 ## incubation ##
 incubationTestSampleCycles_min = 0
 incubationTestSampleCycles_max = 50
 incubationTestSampleCycles_dft = 1
 incubationTestIncubatorDrawVolume_min = 0 #mL
-incubationTestIncubatorDrawVolume_max = 2000 #mL
+incubationTestIncubatorDrawVolume_max = 1950 #mL
 incubationTestIncubatorDrawVolume_dft = 1000 #mL
 incubationTestInjectorDrawVolume_min = 0 #mL
-incubationTestInjectorDrawVolume_max = 2000 #mL
+incubationTestInjectorDrawVolume_max = 500 #mL
 incubationTestInjectorDrawVolume_dft = 0 #mLc
 incubationTestWaitBetweenStudies_min = 0 #sec
 incubationTestWaitBetweenStudies_max = 200000 #sec
