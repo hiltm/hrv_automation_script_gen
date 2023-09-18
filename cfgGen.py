4f3f54f625c43c904a9ac0f6ad490b1df5c42fbc
Michael Hilt <49827546+hiltm@users.noreply.github.com>
2023-09-18 17:19:11 -0400

    fix for line feed/carriage return expectations for PIC program

--- cfgGen.py
+++ cfgGen.py
@@ -151,19 +151,19 @@ def init_cfg():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     f.write("#"+dt_string)
-    f.write("\n\r")
+    f.write("\n")
     f.write("VB:1")                             # set verbose mode high to log all that happens, hard-coding on
-    f.write("\n\r")
+    f.write("\n")
     f.write("TIM")                             # set Mclane time from PIC
-    f.write("\n\r")
+    f.write("\n")
     print("Specify wait time in minutes to allow for instrument deployment, range is between "+str(params.deploymentWaitTime_min)+
           " and "+str(params.deploymentWaitTime_max)+". Default is "+str(params.deploymentWaitTime_dft))
     deploy_waittime = int_check("flush_waittime", params.deploymentWaitTime_min, params.deploymentWaitTime_max, params.deploymentWaitTime_dft)
     if deploy_waittime > 0:
         f.write("#Wait for deployment "+str(deploy_waittime)+" minutes")
-        f.write("\n\r")
+        f.write("\n")
         f.write("wA:"+str(deploy_waittime))     # wait for x minutes
-        f.write("\n\r")
+        f.write("\n")
     print("Note: this is a one-time setting recording physical parameters of the sytem. Specify physical total injector volume in mL, range is between "
           +str(params.injectorPhysicalVolume_min)+" and "+str(params.injectorPhysicalVolume_max)+". Default is "+str(params.injectorPhysicalVolume_dft))
     tV=int_check("tV", params.injectorPhysicalVolume_min, params.injectorPhysicalVolume_max, params.injectorPhysicalVolume_dft)
@@ -171,44 +171,47 @@ def init_cfg():
           +str(params.incubatorPhysicalVolume_min)+" and "+str(params.incubatorPhysicalVolume_max)+". Default is "+str(params.incubatorPhysicalVolume_dft))
     iV=int_check("iV", params.incubatorPhysicalVolume_min, params.incubatorPhysicalVolume_max, params.incubatorPhysicalVolume_dft)
     f.write("tV:"+str(tV))                  # specify physical volume of instrument injection chamber
-    f.write("\n\r")
+    f.write("\n")
     f.write("iV:"+str(iV))                  # specify physical volume of instrument incubation chamber
-    f.write("\n\r")
+    f.write("\n")
     f.write("wHp")                          # go to HOME port to start
-    f.write("\n\r")
+    f.write("\n")
+
+def post_experiment_zero():
+    print("todo")   # TODO
             
 def flush():
     flush_waittime = 0                      # pre-defining
     f.write("#Incubator pre-flush")
-    f.write("\n\r")
+    f.write("\n")
     print("Specify flush cycles for incubator, range is between "+str(params.flushCycles_min)+" and "+str(params.flushCycles_max)+". Default is "+str(params.flushCycles_dft))
     flush_cycles = int_check("flush_cycles", params.flushCycles_min, params.flushCycles_max, params.flushCycles_dft)
     f.write("rP:"+str(flush_cycles))        #repeat for flush_cycles times
-    f.write("\n\r")
+    f.write("\n")
     for x in range(flush_cycles):
         f.write("#Flush cycle "+str(x+1))
-        f.write("\n\r")
+        f.write("\n")
         print("Specify wait time in seconds between flush cycles for flush cycle "+str(x)+", range is between "+str(params.flushWaittime_min)+
               " and "+str(params.flushWaittime_max)+". Default is "+str(params.flushWaittime_dft))
         flush_waittime = int_check("flush_waittime", params.flushWaittime_min, params.flushWaittime_max, params.flushWaittime_dft)
         print("Specify flush amount in mL, range is between "+str(params.flushAmount_min)+" and "+str(params.flushAmount_max)+". Default is "+str(params.flushAmount_dft))
         flush_amount = int_check("flush_amount", params.flushAmount_min, params.flushAmount_max, params.flushAmount_dft)
         f.write("wHp")                      #wait for home port
-        f.write("\n\r")
+        f.write("\n")
         f.write("gSp")                      #get a prompt from the SID
-        f.write("\n\r")
+        f.write("\n")
         f.write("eP")                       #completely empty incubator
-        f.write("\n\r")
+        f.write("\n")
         f.write("rP:"+str(flush_cycles))    #loop flush_cycles times
-        f.write("\n\r")
+        f.write("\n")
         f.write("fV:"+str(flush_amount))     #flush amount
-        f.write("\n\r")
+        f.write("\n")
         f.write("wS:"+str(flush_waittime))   #wait for flush_waittime seconds
-        f.write("\n\r")
+        f.write("\n")
         f.write("eP")                        # completely empty incubator
-        f.write("\n\r")
+        f.write("\n")
     f.write("eRpn")                      # end loop
-    f.write("\n\r")
+    f.write("\n")
     
     time = get_est_runtime() + params.emptyIncubationChamberTime * flush_cycles + flush_waittime * flush_cycles
     set_est_runtime(time)
@@ -218,7 +221,7 @@ def incubation():
     using_injector = False
     ports = []
     f.write("#incubation study")
-    f.write("\n\r")
+    f.write("\n")
 
              ### intake ###
     while not(valid_response):
@@ -234,10 +237,10 @@ def incubation():
             valid_response = True
     if using_injector:
         f.write("fT:"+str(fV)+","+str(iT))                      #fill incubator nnnn & tt volume tracer mL
-        f.write("\n\r")
+        f.write("\n")
     else:
         f.write("fV:"+str(fV))                               #fill incubator nnnn mL 
-        f.write("\n\r")
+        f.write("\n")
     intake = iT + fV                        # intake is total within incubation chamber, sum of injector and incbuator draw volumes
     set_intake(intake)                          # setting global to track intake volume
     print("INTAKE is "+str(intake))
@@ -272,17 +275,17 @@ def incubation():
         if volume_divided_evenly:
             incubationTestSampleVolume = outtake / timepoint_samples
             f.write("eV:"+str(round(incubationTestSampleVolume,2)))         #sample volume
-            f.write("\n\r")
+            f.write("\n")
         else:
             print("Specify amount of sample volume to pump through PORT  "+str(ports[x])+" ,range is between "+str(params.incubationTestSampleVolume_min)+
                   " and "+str(params.incubationTestSampleVolume_max)+". Default is "+str(params.incubationTestSampleVolume_dft))
             incubationTestSampleVolume = int_check("incubationTestSampleVolume", params.incubationTestSampleVolume_min, params.incubationTestSampleVolume_max, params.incubationTestSampleVolume_dft)
             f.write("eV:"+str(incubationTestSampleVolume))         #sample volume
-            f.write("\n\r")
+            f.write("\n")
             print("SUBSAMPLE is "+str(incubationTestSampleVolume))
         if time_divided_evenly:
             f.write("wS:"+str(round(time_between_samples,2)))                #wait for X seconds
-            f.write("\n\r")
+            f.write("\n")
             time = get_est_runtime() + timepoint_samples * params.fillFilterTime + timepoint_samples * time_between_samples
             set_est_runtime(time)
         else:
@@ -290,11 +293,11 @@ def incubation():
                   " and "+str(params.incubationTestSampleVolume_max)+". Default is "+str(params.incubationTestSampleVolume_dft))
             incubationTestSampleWaitTime = int_check("incubationTestSampleWaitTime", params.incubationTestSampleVolume_min, params.incubationTestSampleVolume_max, params.incubationTestSampleVolume_dft)
             f.write("wS:"+str(incubationTestSampleWaitTime))       #wait for X seconds
-            f.write("\n\r")
+            f.write("\n")
             time = get_est_runtime() + timepoint_samples * params.fillFilterTime + timepoint_samples * incubationTestSampleWaitTime
             set_est_runtime(time)
     if volume_divided_evenly:
-        print("SUBSAMPLE for all "+str(timepoint_samples)+ " is "+round(str(incubationTestSampleVolume),2))
+        print("SUBSAMPLE for all "+str(timepoint_samples)+ " is "+str(round(incubationTestSampleVolume,2)))
         
         
 def wait_for_next_experiment():
@@ -302,28 +305,28 @@ def wait_for_next_experiment():
           " and "+str(params.experimentWaitTime_max)+". Default is "+str(params.experimentWaitTime_dft))
     experiment_wait_time=int_check("experiment_wait_time", params.experimentWaitTime_min, params.experimentWaitTime_max, params.experimentWaitTime_dft)
     f.write("#POST EXPERIMENT")
-    f.write("\n\r")
+    f.write("\n")
     f.write("#INTAKE "+str(get_intake())+" mL")      #report intake volume
-    f.write("\n\r")
+    f.write("\n")
     f.write("#OUTTAKE "+str(get_outtake())+" mL")    #report outtake volume
-    f.write("\n\r")
+    f.write("\n")
     f.write("wHp")                              #wait for home port
-    f.write("\n\r")
+    f.write("\n")
     f.write("eP")                                #completely empty incubator
-    f.write("\n\r")
+    f.write("\n")
     f.write("wA:"+str(experiment_wait_time))    #wait for X minutes
-    f.write("\n\r")
+    f.write("\n")
     time = get_est_runtime() + experiment_wait_time * 60 #convert to seconds
     set_est_runtime(time)
 
 def config_summary():
-    f.write("\n\r")
+    f.write("\n")
     f.write("#SUMMARY")
-    f.write("\n\r")
+    f.write("\n")
     f.write("#"+str(get_est_runtime())+" SECONDS")
-    f.write("\n\r")
+    f.write("\n")
     f.write("#PORTS IN USE ARE "+str(get_stored_ports()))
-    f.write("\n\r")
+    f.write("\n")
     print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
     print("Experiment Summary")
     print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
@@ -346,7 +349,7 @@ with open(filename, "w") as f:
     experiments = int_check("experiments", params.experiments_min, params.experiments_max, params.experiments_dft)
     for x in range(experiments):
         #TODO function for reading if chamber is empty
-        f.write("#EXPERIMENT "+str(x+1)+"\n\r")
+        f.write("#EXPERIMENT "+str(x+1)+"\n")
         print(" ")
         print("#####################################")
         print("Incubator Pre-Flush Parameters")
@@ -365,9 +368,9 @@ with open(filename, "w") as f:
         print(" ")
     config_summary()
     f.write("eNd")                              #end of script
-    f.write("\n\r")
+    f.write("\n")
     f.write("#END")                              #end of script
-    f.write("\n\r")
+    f.write("\n")
 
 
     f.close()
