fee25c7e4c91ecce03e5f6e63db8dba1b269f337
Michael Hilt <49827546+hiltm@users.noreply.github.com>
2024-03-22 12:40:39 -0400

    handle any other inputs

--- main.py
+++ main.py
@@ -4,16 +4,29 @@
 import params
 import subprocess
 
+valid_response = False
+
+def print_valid_inputs():
+    print("1 - incubation study")
+    print("2 - filtration study")
+    print("3 - post-swim clean script")
+
 print("Please select which automation script to generate")
-print("1 - incubation study")
-print("2 - filtration study")
-print("3 - post-swim clean script")
+print_valid_inputs()
 #selection = cfgGen.int_check("selection", 0, 4, 0)
-selection = input(": ")
-if selection == '1':
-    subprocess.run(["python", "inc_study.py"])
-elif selection == '2':
-    subprocess.run(["python", "filt_study.py"])
-elif selection == '3':
-    subprocess.run(["python", "post_swim_clean.py"])
-#TODO handle any other inputs
+
+while not(valid_response):
+    selection = input(": ")
+    if selection == '1':
+        valid_response = True
+        subprocess.run(["python", "inc_study.py"])
+    elif selection == '2':
+        valid_response = True
+        subprocess.run(["python", "filt_study.py"])
+    elif selection == '3':
+        valid_response = True
+        subprocess.run(["python", "post_swim_clean.py"])
+    else:
+        print("Please enter a valid input")
+        print_valid_inputs()
+        continue
