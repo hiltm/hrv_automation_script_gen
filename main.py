39d8fef1a6c7546a5dc7a0c0f58bc38057b08c1c
Michael Hilt <49827546+hiltm@users.noreply.github.com>
2024-03-22 12:46:43 -0400

    instructions on how to use script

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#TODO add introduction description
-
-#import cfgGen
-import params
 import subprocess
 
 valid_response = False
@@ -10,6 +6,16 @@ def print_valid_inputs():
     print("1 - incubation study")
     print("2 - filtration study")
     print("3 - post-swim clean script")
+
+print(">>>>>>>>>>> HOW TO USE THIS SCRIPT <<<<<<<<<<<<<<<<")
+print(" -  Please select the type of study to automate below")
+print(" -  This will walk through various prompts for experiment control")
+print(" -  Leaving blank and pressing Enter will use the default value")
+print(" -  This will walk through various prompts for experiment control")
+print(" -  The output is 'msconfig.cfg' This naming is unique to the PIC code")
+print(" -  Utilize 'hrv_file_transfer' to send the automation script to the PIC")
+print(">>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+print(" ")
 
 print("Please select which automation script to generate")
 print_valid_inputs()
